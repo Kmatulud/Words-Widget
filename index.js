60bbe7ff9606cbc5ec3ccbc477bcafe50d33669a
kmatulud <princessmakgotso@gmail.com>
2022-03-15 14:14:38 +0200

    linked factory function in html

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const inputElem = document.querySelector("#userInput");
 const submitElem = document.querySelector("#submit");
+const msg = document.querySelector("#message");
+
 const wordsCountFunc = WordsCountFactory()
 
 const wordsCount = () => {
-    
+
     const userValue = inputElem.value;
     wordsCountFunc.setWords(userValue);
-    wordsCountFunc.getWords();
-
+    msg.innerHTML = wordsCountFunc.getWords();
 }
 
 submitElem.addEventListener("click", wordsCount);
